46cb7ee027b035ac1e60b195b7675970d66d5a0f
alivo18 <438003676@qq.com>
2022-07-06 22:28:52 +0800

    first

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   //webpack的5个重要概念
+//   // entry:'',
+//   // output:{},
+//   // module:{
+//   //   rules:[]
+//   // },
+//   // plugins:{},
+//   // mode:''
+
+//   // 禁用eslint
+//   lintOnSave:false
+// })
+module.exports={
+  // 禁用eslint
+  lintOnSave:false
+}
